5f04abee65e49a076a026396988938b1cc21ca7d
Pablo Coral <pablostuff@Pablos-MacBook-Pro.local>
2025-12-11 13:02:04 -0500

    Commit before changing approach to passing values to diff eq; Realized that since I don't have an analytical equation (kinda obvious) I can't just do f(t, y) and expect it to work since I don't have y(t). Will rely on autonomous ODEs

--- dynamics_engine.py
+++ dynamics_engine.py
@@ -39,56 +39,93 @@ class DynamicsEngine:
         # Use differential equation! Can be ANY standard form ODE: dx/dt = f(x, t)
 
         # catch errors better; no initial condition => RKF45 will not work
+
+        """"
         try: #catch errors if function is not callable object 
-            lambda func: self.f(initial_conditions[0], initial_conditions[1])
+            self.f(initial_conditions[0], initial_conditions[1])
         except Exception as e: #exception is a keyword
             raise ValueError("The function provided is not callable. Please provide a valid function f(x, t).") from e
-        
+        """
+
         #extract initial & final conditions: 
         t_0 = initial_conditions[0]
         t_end = duration
         y_0 = initial_conditions[1]
+
+        #initialize return or plot arrays
+        t_pts = []
+        y_pts = []
+        s_factors = [1] #keep track to visualize later on. S here is INITIAL!
         
-        ## Code my own RK4 or maybe even a more advanced plotting method.--> MAKE MY OWN RKF45 so I can finally understand how it works    
+        ##mMAKE MY OWN RKF45 so I can finally understand how it works    
         ## Things to learn: f(t, y) returns a SLOPE at any point. Thus we can multiply by h or 1/2 h to get different solutions (slopes can be thought of as unit vectors, h is just length)
 
         #first get number of necessary steps to satisfy duration with dt (boundary contidion)
         #generate linearly spaced time points from 0 to duration with step dt
 
+        ##INITIAL CONDITIONS ----------------------------------------
+
         #ONE DAY I could try to implement a 'run until convergence or divergence' method, but for now just do fixed duration
-        time_steps = np.linspace(t_0,t_end,self.dt) #linspace is inclusive of start and endpoints
+
+        num_pts = int((t_end - t_0) / self.dt) + 1 #+1 to include endpoint
+        time_steps = np.linspace(t_0,t_end,num_pts) #need to include num_pts casted to int 
+        #linspace is inclusive of start and endpoints. ALSO, 3d arg is NUMBER OF POINTS, not step size
 
         #calculate necessary coefficients
         y_k = y_0 #initial condition NECESSARY     
 
         for t_k in time_steps:
+
+            ##LOCAL INITIAL CONDITIONS -----------------------------
+
+            e = 1.0 #error starts at 100%
+            s = s_factors[-1] #scaling factor starts at 1 ONLY on first step; Last accepted value of S is kept for the next run
+
+            while True: #for every step, run the calculation every time until the tolerance req is met
+
+                k1 = s * self.dt * self.f(t_k, y_k)
+                #print(f'k1: {k1}')
+                k2 = s * self.dt * self.f(t_k + (1/4)*self.dt, y_k + (1/4)*k1)
+                k3 = s * self.dt * self.f(t_k + (3/8)*self.dt, y_k + (3/32)*k1 + (9/32)*k2)
+                k4 = s * self.dt * self.f(t_k + (12/13)*self.dt, y_k + (1932/2197)*k1 - (7200/2197)*k2 + (7296/2197)*k3)
+                k5 = s * self.dt * self.f(t_k + self.dt, y_k + (439/216)*k1 - 8*k2 + (3680/513)*k3 - (845/4104)*k4)
+                k6 = s * self.dt * self.f(t_k + (1/2)*self.dt, y_k - (8/27)*k1 + 2*k2 - (3544/2565)*k3 + (1859/4104)*k4 - (11/40)*k5)
+
+                #update y_new using weighted sum of ks. Create z_new for error estimation
+                y_new = y_k + (25/216)*k1 + (1408/2565)*k3 + (2197/4104)*k4 - (1/5)*k5
+                z_new = y_k + (16/135)*k1 + (6656/12825)*k3 + (28561/56430)*k4 - (9/50)*k5 + (2/55)*k6
+
+                #recalculate error to ensure it's within tolerance
+                #print(f'vals y:{y_new}, z: {z_new}')
+                err = np.abs(z_new - y_new)
             
-            k1 = self.dt * self.f(t_k, y_k)
-            k2 = self.dt * self.f(t_k + (1/4)*self.dt, y_k + (1/4)*k1)
-            k3 = self.dt * self.f(t_k + (3/8)*self.dt, y_k + (3/32)*k1 + (9/32)*k2)
-            k4 = self.dt * self.f(t_k + (12/13)*self.dt, y_k + (1932/2197)*k1 - (7200/2197)*k2 + (7296/2197)*k3)
-            k5 = self.dt * self.f(t_k + self.dt, y_k + (439/216)*k1 - 8*k2 + (3680/513)*k3 - (845/4104)*k4)
-            k6 = self.dt * self.f(t_k + (1/2)*self.dt, y_k - (8/27)*k1 + 2*k2 - (3544/2565)*k3 + (1859/4104)*k4 - (11/40)*k5)
+                
+                if err <= tolerance: 
+                    y_k = y_new
+                    t_pts.append(t_k)
+                    y_pts.append(y_k)
+                    break
+                else: 
+                    s = 0.84 * (tolerance / err)**0.25 #formula for s given by Numerical Methods using MATLAB, 4th edition, Jaan Kiusalaas
+                    s_factors.append(s)
 
-            #update y_new using weighted sum of ks. Create z_new for error estimation
-            y_new = y_k + (25/216)*k1 + (1408/2565)*k3 + (2197/4104)*k4 - (1/5)*k5
-            z_new = y_k + (16/135)*k1 + (6656/12825)*k3 + (28561/56430)*k4 - (9/50)*k5 + (2/55)*k6
+                # may have to do some sort of time-out in case tolerance is never met
+                #if error is less than tolerance, loop will break naturally
+                #if error is still greater than tolerance, step size needs to be recalculated
+                  
 
-            #check error & ensure it's within tolerance
-            err = np.abs(z_new - y_new)
-
-            if err <= tolerance: 
 
             #program does NOT 'continue' if error is exceeded (step has to be adaptive from the beginning) 
             ## y_new is a 4th order estimate (up to k5), z_new is a 5th order estimate (up to k6). We can compare them to determine the next best step size
 
         if return_as == 'plot':
 
-            plt.plot(t_points, x_points)
+            plt.plot(t_pts, y_pts)
             plt.xlabel('Time')
             plt.ylabel('Response')
             plt.title('System Response Over Time')
             plt.grid()
+            plt.plot(t_pts, s_factors)
             plt.show()
             return None
         
--- playground.py
+++ playground.py
@@ -1,6 +1,29 @@
 from dynamics_engine import DynamicsEngine
 
-f = lambda x, t: -2*x**2 + 1
+
+"""
+y is ANOTHER LAMBDA FUNCTION:
+
+-> y(t) = a separate equation
+- I can or cannot know y(t) analytically which is fine, so long as I have real INITIAL CONDITIONS for every y^n(t) and how it couples with dy/dt
+- And also, if I knew y(t) analytically why would I need to do a numerical solver...
+
+Basically: 
+1. Know the relationship that an unknown function, y(t) makes with its derivative dy/dt
+2. Know that the function y(t) exists at some initial condition t=0 for example
+3. Get the value of y(t) at that initial condition, y(0) = 1 for example (y(0) = 0 is invalid for a SOME diff eq's)
+4. Now I have the IC's for my differential equation : f(y,t) = f(1, 0) and I can use this to numerically find y(t
+)
+-> f(y(t), t) = the differential equation whih DEPENDS on function values of y and outputs a rate 
+
+"""
+
+
+f = lambda t, y: -2*(1/(y**(2))) + 1 #& y -> y(t)
 
 engine = DynamicsEngine(f, dt=0.1)
-engine.get_response(initial_conditions=[0, 0], duration=10, return_as='plot')
+
+engine.get_response(initial_conditions=[0, 1], duration=10, return_as='plot')
+
+#FOUND A SUPER SILLY ERROR: If my inital conditions are 0,0 then x=0 and 1/x^2 = 0...
+#Must be able to handle singularities
